b01c9f89690591c86a0e39ea61af7bd0327250e5
sune.lindmark <sune.lindmark@elev.ga.ntig.se>
2019-09-03 11:00:22 +0200

    början på loggbok

--- src/logbok.java
+++ src/logbok.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class logbok {
     public static void main(String[] args){
         System.out.println(
@@ -8,5 +10,8 @@ public class logbok {
                 "5 Läs in \n" +
                 "6 Avsluta");
 
+
+        Scanner visa = new Scanner(System.in);
+            
     }
 }
