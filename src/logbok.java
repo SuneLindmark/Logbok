0a42c6140ab22a666bd04c021f8f127a5566ecef
sune.lindmark <sune.lindmark@elev.ga.ntig.se>
2019-09-03 09:32:17 +0200

    uppdatering

--- src/logbok.java
+++ src/logbok.java
@@ -1,2 +1,12 @@
 public class logbok {
+    public static void main(String[] args){
+        System.out.println(
+                "1 Visa \n" +
+                "2 Lägg till post \n" +
+                "3 Uppdatera \n" +
+                "4 Spara \n" +
+                "5 Läs in \n" +
+                "6 Avsluta");
+
+    }
 }
