2eadddcd829bf1d4fb5b4ab0c521a437fb5a3eea
sune.lindmark <sune.lindmark@elev.ga.ntig.se>
2019-09-10 08:55:36 +0200

    LogEntry added

---
+++ src/LogEntry.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public class LogEntry {
+    private Date createdAt;
+    private Date updatedAt;
+    private String message;
+
+    public LogEntry(String message) {
+        this.message = message;
+        createdAt = new Date();
+        updatedAt = new Date();
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "createdAt:" + createdAt +
+                ", updatedAt:" + updatedAt +
+                ", message:'" + message + '\'' +
+                '}';
+    }
+
+    public void update(String newMessage) {
+
+    }
+}
